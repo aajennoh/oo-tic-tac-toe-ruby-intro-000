18063e35ceba1fe3188554ac25bcedd160797a8f
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-11 18:53:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,6 @@
 class TicTacToe
   def initialize(board=" ")
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    board || Array.new(9, " ")
+    @board = Array.new(9, " ")
   end
 
 WIN_COMBINATIONS = [

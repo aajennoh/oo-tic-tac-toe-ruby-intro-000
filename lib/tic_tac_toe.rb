b72be9f86c08163c2ccf3eb95a9c50c61b3cf876
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-11 19:11:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,15 +1,5 @@
 class TicTacToe
 
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
 
 def initialize
   @board = Array.new(9, " ")
@@ -137,5 +127,38 @@ def play
     puts "Cat's Game!"
   end
 end
+
+end
+
+
+class TicTacToe
+  def initialize(board =" ")
+    @board = (board || Array.new(9, " "))
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def display_board(board)
+    @board = board || Array.new(9, " ")
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i
+  end
+
 
 end

e52d09b5838ab9bc601de36a95dd1c5e8c4847d5
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-11 19:15:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,8 +16,7 @@ WIN_COMBINATIONS = [
   [2,4,6] # Right to left diagonal
 ]
 
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board(board)
+def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "

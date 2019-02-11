e569d325ad632ac9a843480b3cc0b4408fd5f9e5
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-11 19:02:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,7 @@ class TicTacToe
   end
 
   def display_board
-    @board = board
+    @board = Array.new(9, " ")
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "

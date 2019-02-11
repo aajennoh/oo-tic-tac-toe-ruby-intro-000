103b460bdb22e3a6ed46c270d8ecd1a9aa414c76
JENNIFER OH <ohjennifer.90@gmail.com>
2019-02-11 19:18:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,16 +56,16 @@ end
 #  index.between?(0,8) && !position_taken?(board, index)
 #end
 
-def turn(board)
+def turn
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index)
-    player_character = current_player(board)
-    move(board, index, player_character)
-    display_board(board)
+  if valid_move?(index)
+    player_character = current_player
+    move(index, character)
+    display_board
   else
-    turn(board)
+    turn
   end
 end
 
